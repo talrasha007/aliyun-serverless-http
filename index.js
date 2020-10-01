bf544523754a59908d7c9191a8daf305aa0e6caa
Tal <talrasha007@gmail.com>
2020-10-01 21:17:40 +0800

    convert query parameters.

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 module.exports = (serverlessHandler, { log = true } = {}) => {
   return (event, context, callback) => {
+    if (Buffer.isBuffer(event)) event = JSON.parse(event.toString());
+    event.queryStringParameters = event.queryParameters;
+
     if (log) console.log("request: " + JSON.stringify(event));
     serverlessHandler(event, context)
       .then(res => callback(null, res))
