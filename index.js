986cf8457719173a6e018cecc34206a682a6894b
Tal <talrasha007@gmail.com>
2020-10-01 19:40:11 +0800

    log opt

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
-module.exports = serverlessHandler => {
+module.exports = (serverlessHandler, { log = true }) => {
   return (event, context, callback) => {
+    if (log) console.log("request: " + JSON.stringify(event));
     serverlessHandler(event, context)
       .then(res => callback(null, res))
       .catch(e => callback(e || 'Unknown error.'));
